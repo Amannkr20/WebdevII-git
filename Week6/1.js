76fc240b8f52b9b373551fabea32365449ef0666
Aman Kumar <amann.kr20@gmail.com>
2026-02-26 09:52:46 +0530

    “https://github.com/Amannkr20”

---
+++ Week6/1.js
@@ -0,0 +1,44 @@
+// promise is most important for mid term 
+
+
+
+// 7. clousers ->  function + its lexical scope of varibale 
+//  Encapsulation in js is achived by clousers 
+// function outer(){
+// let count = 0;
+// return function inner (){
+//     count++;
+//     console.log(count);
+// }
+// }
+
+// let ans = outer();
+// ans()
+// ans()
+// ans()
+
+
+// //  8.Curring
+// function add(a){
+//     return function(b){
+//         return function(c){
+//             return function(d){
+//                 return a+b+c+d;
+//             }
+//         }
+//     }
+// }
+// let ans = add(1)(2)(3)(4);
+// console.log(ans);
+
+
+//  // 9. arrow function
+//  const add = (a) => (b) => (c) => (d) => a+b+c+d;
+//         {
+//                 return a+b+c+d;
+//             }
+// let ans = add(1)(2)(3)(4);
+// console.log(ans);
+
+
+// 10. promise
